b17ccabd9e8deb57fcac1fc2184a712dc00690cd
kfmiran71 <billyklutch@yahoo.com>
2026-05-03 22:44:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-p.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
