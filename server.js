fe50adf791bdb757d8cf06aae2e0b83a457d1662
kfmiran71 <billyklutch@yahoo.com>
2026-05-04 12:12:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -36,8 +36,13 @@ app.get("/push-arrivals", async (req, res) => {
       },
       body: JSON.stringify({
         appID: "TYenWzXz52pcp3wCTXG6",
-        mutations: arrivals.map(arrival => ({
-  kind: "add-row-to-table",
+        mutations: [
+     {
+     kind: "delete-all-rows-from-table",
+     tableName: "native-table-d3UgJzNMFLdWdcIIc8AP"
+   },
+       ...arrivals.map(arrival => ({   
+          kind: "add-row-to-table",
   tableName: "native-table-d3UgJzNMFLdWdcIIc8AP",
   columnValues: {
     "Name": arrival.platformId,
