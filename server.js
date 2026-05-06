e55dabaac2cb86506bb5963e664b45d3e157d6a0
kfmiran71 <billyklutch@yahoo.com>
2026-05-06 07:54:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -150,6 +150,24 @@ limitedArrivals.forEach((a, i) => {
     direction: a.direction
   });
 });
+
+    await fetch("https://api.glideapp.io/api/function/mutateTables", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer d25737fc-2ba6-4dfa-bcc1-0b1150680e14"
+  },
+  body: JSON.stringify({
+    appID: "TYenWzXz52pcp3wCTXG6",
+    mutations: [
+      {
+        kind: "delete-all-rows-from-table",
+        tableName: "native-table-d3UgJzNMFLdWdcIIc8AP"
+      }
+    ]
+  })
+});
+    
     const runId = Date.now().toString();
     const response = await fetch("https://api.glideapp.io/api/function/mutateTables", {
       method: "POST",
