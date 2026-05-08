164f9c027dff4c852edb1ab0b49cbe11fba36a5b
kfmiran71 <billyklutch@yahoo.com>
2026-05-07 21:23:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ import fs from "fs";
 
 
 import path from "path";
-
+import { fileURLToPath } from "url";
 const stationsPath = path.resolve("./stations.json");
 const STATION_MAP = JSON.parse(fs.readFileSync(stationsPath, "utf-8"));
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
