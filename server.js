3471e12f3f9a3dfd15081c2ee249ecd1c68c1d43
kfmiran71 <billyklutch@yahoo.com>
2026-05-08 15:04:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -60,43 +60,75 @@ const routeId = req.query.routeId;
 
 let feeds = [];
 
-if (["1","2","3","4","5","6","7"].includes(routeId)) {
+if (!routeId) {
+
+  feeds = [
+    "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs",
+    "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace",
+    "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm",
+    "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw",
+    "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz",
+    "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g",
+    "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l"
+  ];
+
+}
+
+else if (["1","2","3","4","5","6","7"].includes(routeId)) {
+
   feeds = [
     "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs"
   ];
+
 }
 
 else if (["A","C","E"].includes(routeId)) {
+
   feeds = [
     "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace"
   ];
+
 }
 
 else if (["B","D","F","M"].includes(routeId)) {
+
   feeds = [
     "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm"
   ];
+
 }
 
 else if (["N","Q","R","W"].includes(routeId)) {
+
   feeds = [
     "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw"
   ];
+
 }
 
 else if (["J","Z"].includes(routeId)) {
+
   feeds = [
     "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz"
   ];
+
 }
 
 else if (routeId === "G") {
+
   feeds = [
     "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g"
   ];
+
 }
 
 else if (routeId === "L") {
+
+  feeds = [
+    "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l"
+  ];
+
+}
   feeds = [
     "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l"
   ];
